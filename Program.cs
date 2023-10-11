7fca0162060ce66ef1362c9cde68b21a0cc019ad
brenda <bbritob8@gmail.com>
2023-10-11 16:26:12 -0300

    implementando lógica

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System.Text;
+using SistemaDeHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+List<Hospede> hospedes = new List<Hospede>();
+
+Hospede p1 = new Hospede(nome: "Hóspede 1");
+Hospede p2 = new Hospede(nome: "Hóspede 2");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+Suite suite = new Suite(tipoSuite: "Executiva", capacidade: 2, valorDiaria: 40);
+
+Reserva reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
